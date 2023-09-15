883d25a8427449243d86057a0fb28d3cb16f3249
ericka lus <lusericka0@gmail.com>
2023-09-15 14:09:12 +0100

    ajout du frontend au referentiel

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+   <div className="container">
+<h1>TODO App</h1>
+<div className="top">
+  <input type="text" placeholder="Add ToDo..."/>
+</div>
+   </div>
+    </div>
+  );
+}
+
+export default App;
